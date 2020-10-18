fd17b43a5d4fcca3720015bc49590ea25138c4f8
Zepei Cen <zepei.cen@gmail.com>
2020-10-18 15:15:04 -0500

    update

---
+++ code_samples/012 updated-version/Ninja-Level-3/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+type favSport string
+
+func main() {
+	fmt.Println(true && true)
+	fmt.Println(true && false)
+	fmt.Println(true || true)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+}
