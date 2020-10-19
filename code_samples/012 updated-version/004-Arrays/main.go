4c5c093477da03aeea871fb7bca35426228f1f22
Zepei Cen <zepei.cen@gmail.com>
2020-10-18 19:44:59 -0500

    update

--- code_samples/012 updated-version/004-Arrays/main.go
+++ code_samples/012 updated-version/004-Arrays/main.go
@@ -3,15 +3,26 @@ package main
 import "fmt"
 
 func main() {
-	//X := type {values} // composite literal
-	x := []int{4, 5, 7, 8, 42}
-	fmt.Println(len(x))
-	fmt.Println(x[1])
-	fmt.Println(x)
-	fmt.Println(x[1:])
-	fmt.Println(x[1:3])
-
-	for i := 0; i <= 4; i++ {
-		fmt.Println(i, x[i])
+	m := map[string]int{
+		"James":           32,
+		"Miss Moneypenny": 27,
 	}
+	fmt.Println(m)
+	fmt.Println(m["James"])
+	fmt.Println(m["Barnabas"])
+
+	if v, ok := m["Barnabas"]; ok {
+
+		fmt.Println(v)
+		fmt.Println(ok)
+	}
+	for k, v := range m {
+		fmt.Println(k, v)
+
+	}
+	xi := []int{4, 5, 7, 8, 9, 42}
+	for i, v := range xi {
+		fmt.Println(i, v)
+	}
+
 }
