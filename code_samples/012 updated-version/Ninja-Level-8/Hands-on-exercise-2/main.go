5ca328b1b858f46cf1294e39184899ddf06de8ae
Zepei Cen <zepei.cen@gmail.com>
2020-10-28 00:46:47 -0500

    update

---
+++ code_samples/012 updated-version/Ninja-Level-8/Hands-on-exercise-2/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type people []struct {
+	First   string   `json:"First"`
+	Last    string   `json:"Last"`
+	Age     int      `json:"Age"`
+	Sayings []string `json:"Sayings"`
+}
+
+type person struct {
+	First   string
+	Last    string
+	Age     int
+	Sayings []string
+}
+
+func main() {
+	s := `[{"First":"James","Last":"Bond","Age":32,"Sayings":["Shaken, not stirred","Youth is no guarantee of innovation","In his majesty's royal service"]},{"First":"Miss","Last":"Moneypenny","Age":27,"Sayings":["James, it is soo good to see you","Would you like me to take care of that for you, James?","I would really prefer to be a secret agent myself."]},{"First":"M","Last":"Hmmmm","Age":54,"Sayings":["Oh, James. You didn't.","Dear God, what has James done now?","Can someone please tell me where James Bond is?"]}]`
+	fmt.Println(s)
+	var people []person
+	bs := []byte(s)
+
+	err := json.Unmarshal(bs, &people)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	for i, v := range people {
+		fmt.Println("Person:", i)
+		fmt.Println("\t", v.First, v.Last, v.Age)
+		for _, val := range people {
+			fmt.Println("\t", val.Sayings)
+		}
+	}
+
+}
